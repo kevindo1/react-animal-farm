1b991782f11161571c23ec77178112b5377c8eac
Kevin Do <kevind0501@gmail.com>
2021-11-29 15:52:42 -0800

    working animal farm

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,28 @@
 import React from 'react'
 import './Main.css'
+import { animals } from '../../data'
+import { Animal } from '../Animal/Animal'
+import background from '../../background.png'
 
 export default function Main() {
-  return <main></main>
+  return (
+    <main className="background" style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal
+          key={animal.id}
+          name={animal.name}
+          type={animal.type}
+          says={animal.says}
+          left={animal.left}
+          top={animal.top}
+        />
+      ))}
+    </main>
+  )
 }
+
+// name: 'Betsy the Cow',
+//     type: 'cow',
+//     says: 'Mooooooo!',
+//     top: '200px',
+//     left: '50%',
