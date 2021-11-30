d34bc47bf6dc2068f9560859e20e3bec2864987a
Kevin Do <kevind0501@gmail.com>
2021-11-30 12:27:47 -0800

    destructuring props

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Animal.css'
 
-export function Animal(props) {
+export function Animal({ top, left, name, type, says }) {
   return (
-    <div className="animal" style={{ top: props.top, left: props.left }}>
-      <img alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
-      <span className="name">{props.name}</span>
-      <span>{props.type}</span>
-      <span>{props.says}</span>
+    <div className="animal" style={{ top: top, left: left }}>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+      <span className="name">{name}</span>
+      <span>{says}</span>
     </div>
   )
 }
